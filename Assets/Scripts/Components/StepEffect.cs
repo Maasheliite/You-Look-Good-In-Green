134915ed975d742b314e6fc599e7d2d6079174f6
Creonox <Nathan_K@hotmail.de>
2023-02-05 01:02:41 +0200

    Main scene border + stepeffect adjustment

--- Assets/Scripts/Components/StepEffect.cs
+++ Assets/Scripts/Components/StepEffect.cs
@@ -20,19 +20,19 @@ public class StepEffect : MonoBehaviour
     IEnumerator StepCoroutine()
     {
         var stepObject = Instantiate(stepPrefab, transform.position,Quaternion.identity);
-        float rippleTime = 4f;
-        float rippleIntensity = 0.001f;
+        //float rippleTime = 4f;
+        float effectInterval = 0.001f;
         float timer = 0;
 
         float startTime = Time.time;
 
 
-        while (timer < rippleTime)
+        while (stepObject.isPlaying)
         {
             timer += Time.deltaTime;
            /* stepLight.intensity = 1.5f+Mathf.Sin(timer);
             stepLight.spotAngle = Mathf.Lerp(1,maxAngle,timer/ rippleTime);*/
-            yield return new WaitForSeconds(rippleIntensity);
+            yield return new WaitForSeconds(effectInterval);
         }
         GameObject.Destroy(stepObject.gameObject);
         yield break;
