f50b703e6a18270419b624cc539e0bc3cff18523
Lakria <maasheliite24@gmail.com>
2023-06-15 10:05:26 +0300

    end scene

--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameStateManager : MonoBehaviour
 {
@@ -32,5 +33,10 @@ public class GameStateManager : MonoBehaviour
             return true;
         }
         return false;
+    }
+
+    public void LoadEnd()
+    {
+        SceneManager.LoadScene("End scene");
     }
 }
