6d50f60cc662fbfb2b9bc819b63be3ca1c180196
Lakria <maasheliite24@gmail.com>
2023-06-15 07:28:24 +0300

    birds

--- Assets/Lui WIP/Birds.cs
+++ Assets/Lui WIP/Birds.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Nizu.Util.ScriptableObjects;
-
-
+using Nizu.InventorySystem;
 
 public class Birds : MonoBehaviour, IInteractable
 {
@@ -11,13 +10,23 @@ public class Birds : MonoBehaviour, IInteractable
 
     private SpriteRenderer spriteRenderer;
     public SetSelfInactive costElement;
-    public GameCommand stickResource;
-    public float stickCost;
+    public ItemDetails stickResource;
+    public ItemDetails leafResource;
+    public ItemDetails clayResource;
+    public ItemDetails wormResource;
+    public int stickCost;
+    public int leafCost;
+    public int clayCost;
+    public int wormCost;
     public Sprite built;
 
+    public Inventory inventory;
 
     private bool builtBool = false;
 
+    public GameObject mainCanvas;
+    public GameObject BirdMap;
+    private bool mapOpen;
 
     public GameObject getGameObject()
     {
@@ -26,9 +35,22 @@ public class Birds : MonoBehaviour, IInteractable
 
     public void Interact(GameObject actor)
     {
-        if (stickResource.DecreaseValueUntilZero(stickCost))
+        if (builtBool && !mapOpen)
+        {
+            BirdMap.SetActive(true);
+            mainCanvas.SetActive(false);
+
+        }
+        else if (mapOpen)
+        {
+            BirdMap.SetActive(false);
+            mainCanvas.SetActive(true);
+
+        }
+        else if (inventory.removeItemOfType(stickResource, stickCost) && inventory.removeItemOfType(leafResource, leafCost) &&  inventory.removeItemOfType(clayResource, clayCost) && inventory.removeItemOfType(wormResource, wormCost))
         {
             spriteRenderer.sprite = built;
+            builtBool = true;
         }
     }
 
